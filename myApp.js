52b18ebe95ced6c805019940463467c9e278b149
SebMDev <sebmdev@gmail.com>
2022-06-14 08:12:20 +0100

    send file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,14 @@
 let express = require('express');
 let app = express();
 
+// app.get("/", (req, res) => {
+//     res.send("Hello Express")
+// })
+
+const absolutePath = __dirname + views/index.html
+
 app.get("/", (req, res) => {
-    res.send("Hello Express")
+    res.sendFile(absolutePath)
 })
 
 

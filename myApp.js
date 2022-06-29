a04924d186be87016b16a7b8dd0e4c7c471641e4
seb <sebmdev@gmail.com>
2022-06-29 22:33:39 +0100

    add uppercase with environment variable

--- myApp.js
+++ myApp.js
@@ -8,12 +8,11 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-  let message = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
   }
-  console.log(message);
-  res.json({ message: message });
 });
 
 module.exports = app;
